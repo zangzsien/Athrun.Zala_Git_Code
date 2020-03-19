6430bab8914e854467b7745c67418cb61c950770
zangzsien <995000469@qq.ocm>
2020-03-19 17:34:49 +0800

    第一次提交

---
+++ Athrun.Zala_Python_Code/Raspberry_Pi_project/Pi_Cooling_fan_driver.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+#coding:utf8
+
+#自动风扇控制程序，使用wringPi的gpio命令来操作GPIO
+
+import subprocess,time
+
+#控制风扇的GPIO
+FAN_GPIO = 15
+
+subprocess.getstatusoutput('sudo gpio mode ' + str(FAN_GPIO) + ' OUTPUT')
+while True:
+    tmpFile = open('/sys/class/thermal/thermal_zone0/temp')
+    cpu_temp_raw = tmpFile.read()
+    tmpFile.close()
+    cpu_temp = round(float(cpu_temp_raw)/1000,1)
+    print(cpu_temp)
+    #如果温度大于47℃，就启动风扇
+    if cpu_temp >= 47:
+        subprocess.getstatusoutput('sudo gpio write '+str(FAN_GPIO)+' 0')
+     #如果温度小于40`C，就关闭风扇
+    if cpu_temp <= 40 :
+        subprocess.getstatusoutput('sudo gpio write '+str(FAN_GPIO)+' 1')
+    
+    time.sleep(10)
