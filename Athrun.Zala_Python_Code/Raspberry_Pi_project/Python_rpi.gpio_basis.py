38b0bb4ddbd9f893e39213ae80d3884c4d0108a4
zangzsien <995000469@qq.com>
2020-03-21 23:13:50 +0800

    提交一下试试

--- Athrun.Zala_Python_Code/Raspberry_Pi_project/Python_rpi.gpio_basis.py
+++ Athrun.Zala_Python_Code/Raspberry_Pi_project/Python_rpi.gpio_basis.py
@@ -2,7 +2,7 @@ import RPi.GPIO as GPIO
 #导入树莓派的第三方库
 
 #1.使用GPIO库的setmode方法来设置开发板的引脚模式。
-#两种模式：GPIO.BOARD为自然编号 GPIO.BCM 为Broadcom编号
+#  两种模式：GPIO.BOARD为自然编号 GPIO.BCM 为Broadcom编号
 
 GPIO.setmode(GPIO.BOARD)
 
